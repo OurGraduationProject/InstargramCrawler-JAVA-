f1c13b5ffc297801130b78f0d9ebe4065a16fd1d
dongdang-cho <60133320+dongdang-cho@users.noreply.github.com>
2020-03-26 22:12:30 +0900

    로그인 오류시 발생할 예외

---
+++ InstargramCrawler2/src/main/java/model/exception/LoginException.java
@@ -0,0 +1,9 @@
+package model.exception;
+
+public class LoginException extends Exception {
+
+	public LoginException() {
+		// TODO Auto-generated constructor stub
+		super("로그인 과정에 에러가 발생하였습니다.");
+	}
+}
