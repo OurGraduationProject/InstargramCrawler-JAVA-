7ebcc276628da78cddcda59d9e02d69e2e497994
dongdang-cho <60133320+dongdang-cho@users.noreply.github.com>
2020-03-26 22:10:10 +0900

    프로젝트 메타 데이터 로드 클래스 v1.0

---
+++ InstargramCrawler2/src/main/java/util/MetaDataLoader.java
@@ -0,0 +1,52 @@
+package util;
+
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.Properties;
+
+public class MetaDataLoader {
+	
+	private static Properties seleniumProfile = new Properties();
+	private static Properties accountProfile = new Properties();
+	private static Properties dbInfoProfile = new Properties();
+	private static Properties dbCmdProfile = new Properties();
+	static {
+		try {
+			seleniumProfile.load(new FileInputStream("seleniumInfo.properties"));	
+			accountProfile.load(new FileInputStream("account.properties"));
+			dbInfoProfile.load(new FileInputStream("dbinfo.properties"));
+			dbCmdProfile.load(new FileInputStream("dbcmd.properties"));
+			DBConnManager.dbLoading(dbInfoProfile);
+		} catch (FileNotFoundException e) {
+			// TODO Auto-generated catch block
+			System.err.println("메타 데이터 파일이 존재하지 않습니다.");
+			e.printStackTrace();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} 
+		
+	}
+
+	/**
+	 * @return the seleniumProfile
+	 */
+	public static Properties getSeleniumProfile() {
+		return seleniumProfile;
+	}
+	/**
+	 * @return the idProfile
+	 */
+	public static Properties getIdProfile() {
+		return accountProfile;
+	}
+	public static Properties getDbCmdProfile() {
+		return dbCmdProfile;
+	}
+
+	
+	
+	
+}
